8cd65bf3b52a7867efe924d519d1649080a2f852
Sachin Ahuja <sachinahj@gmail.com>
2016-08-16 06:48:32 -0500

    change limit back to 20

--- scripts/yelp/grabAllCategoriesForCity.js
+++ scripts/yelp/grabAllCategoriesForCity.js
@@ -25,7 +25,7 @@ const _getInitialParams = (city, filteredCategoryIndex) => {
     return {
       location: `${city.name},${city.state}`,
       sort_by: 'rating',
-      limit: 50,
+      limit: 20,
       offset: 0,
       categories: filteredCategories[filteredCategoryIndex].alias,
     };
