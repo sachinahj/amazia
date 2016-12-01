610475b215b745e5b8c93a0f9f1ccc6dfb579737
Sachin Ahuja <sachinahj@gmail.com>
2016-12-01 02:15:48 -0600

    add level4 to level1 query

--- savedQueries/find_category_count_in_city.sql
+++ savedQueries/find_category_count_in_city.sql
@@ -13,3 +13,14 @@ where city.id=1
 group by c.alias
 order by count desc
 ;
+
+
+select b.cityId, yc.alias, yct.level4, yct.level1, count(yct.level1) as count
+from yelpBusiness b
+join yelpBusinessCategory bc on b.id=bc.businessId
+join yelpCategory yc on yc.id=bc.categoryId
+join yelpCategoryTree yct on yct.level4=yc.alias
+where b.cityId=1
+group by yct.level1
+order by count desc
+;
